57e8c28a7d5d9191d61a5705b28e24ac58554ca8
peanut1996 <peanut_1996@163.com>
2019-07-10 18:16:00 +0800

    加入Nprogress进度条

--- src/main.js
+++ src/main.js
@@ -3,9 +3,24 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入nprogress
+import NProgress from 'nprogress' // 进度条
+import 'nprogress/nprogress.css' // 这个样式必须引入
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+
+NProgress.inc(0.2)
+NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
 
 new Vue({
   router,
